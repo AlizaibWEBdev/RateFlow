0231955f0708692c739affeae93fc50e1f5210a0
Ali zaib <129104458+AlizaibWEBdev@users.noreply.github.com>
2025-07-20 01:08:41 -0700

    add cors

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
-
+app.use(cors());
 app.set('view engine', 'ejs');
-
 app.set("views", __dirname + "/views");
 app.use(express.static( __dirname + "/public"))
-
 app.get('/', (req, res) => {
     res.render('index');    
 });
